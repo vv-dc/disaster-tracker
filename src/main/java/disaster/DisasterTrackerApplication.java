e8d9f4b58ee6f541cd0258d8aec9864710b40d17
Vadym Kinchur <vkincur@gmail.com>
2022-06-19 12:31:58 +0300

    Add gitignore

---
+++ src/main/java/disaster/DisasterTrackerApplication.java
@@ -0,0 +1,12 @@
+package disaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class DisasterTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisasterTrackerApplication.class, args);
+    }
+}
---
+++ src/main/java/disaster/config/RoutingConfig.java
@@ -0,0 +1,5 @@
+package disaster.config;
+
+public class RoutingConfig {
+
+}
