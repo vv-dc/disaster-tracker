17483c1dcec132a0741f7baf60197152c73ff868
Vadym Kinchur <vkincur@gmail.com>
2022-06-21 23:55:15 +0300

    Make repository monorepo, initialize frontend

--- backend/src/main/java/disaster/controller/platform/PlatformRestController.java
+++ backend/src/main/java/disaster/controller/platform/PlatformRestController.java
@@ -1,15 +1,14 @@
 package disaster.controller.platform;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class PlatformRestController {
 
     @GetMapping("/platform/ping")
-    @ResponseBody
-    public String pingRoute() {
-        return "pong";
+    public Mono<String> pingRoute() {
+        return Mono.just("pong");
     }
 }
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+export const App = () => {
+  return (
+    <h1>Hi there</h1>
+  );
+}
+
+export default App;
